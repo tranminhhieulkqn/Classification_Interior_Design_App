7a0ff883f44dbf285b6e0b14c64b868b834e0740
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2020-12-20 13:47:36 +0700

    * Change select in dropdown. * Update file package installer.

--- lib/screens/results/results_screen.dart
+++ lib/screens/results/results_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 import 'package:interior_app/consttants.dart';
 import 'package:interior_app/widgets/app_clipper.dart';
@@ -6,7 +5,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:interior_app/widgets/results_chart.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+// import 'dart:async';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ResultsScreen extends StatefulWidget {
   final File image;
@@ -20,10 +21,11 @@ class ResultsScreen extends StatefulWidget {
 
 class _ResultsScreenState extends State<ResultsScreen> {
   List<String> _Models = ["Model 1", "Model 2", "Model 3"];
-  String _selectedModel = "Xception";
+  String _selectedModel = "Model 1";
+  String _selectedModelConverted = 'Xception';
 
-  String _SelectedModel(String model){
-    switch(model){
+  String _SelectedModel(String model) {
+    switch (model) {
       case 'Model 1':
         return 'Xception';
       case 'Model 2':
@@ -65,166 +67,170 @@ class _ResultsScreenState extends State<ResultsScreen> {
       },
       child: SingleChildScrollView(
           child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: size.height,
-                width: size.width,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      top: 0,
-                      child: Stack(
-                        children: <Widget>[
-                          Container(
-                            height: size.height,
-                            width: size.width,
-                            color: Colors.white,
-                          ),
-                          Container(
-                            height: size.height,
-                            width: size.width,
-                            color: Colors.white,
-                          ),
-                          Container(
-                            height: size.height,
-                            width: size.width,
-                            color: Colors.white,
-                          ),
-                          Opacity(
-                            opacity: .7,
-                            child: Container(
-                              height: size.height,
-                              width: size.width,
-                              decoration: BoxDecoration(
-                                // borderRadius:
-                                // BorderRadius.all(Radius.circular(30)),
-                                  image: DecorationImage(
-                                      image: FileImage(widget.image),
-                                      fit: BoxFit.cover)),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      child: Container(
-                        height: size.height * .7,
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            width: size.width,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 0,
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: size.height,
                         width: size.width,
-                        child: ClipPath(
-                          clipper: AppClipper(
-                              cornerSize: 50,
-                              diagonalHeight: 150,
-                              roundedBottom: false),
-                          child: Container(
-                            color: Colors.white,
-                            padding: EdgeInsets.only(top: 100),
-                          ),
-                        ),
+                        color: Colors.white,
                       ),
-                    ),
-                    Positioned(
-                        bottom: size.height * .43,
-                        right: size.width * .05,
+                      Container(
+                        height: size.height,
+                        width: size.width,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        height: size.height,
+                        width: size.width,
+                        color: Colors.white,
+                      ),
+                      Opacity(
+                        opacity: .7,
                         child: Container(
-                            alignment: Alignment.center,
-                            height: size.width * (1.6 / 3),
-                            width: size.width * (1.6 / 3),
-                            child: DottedBorder(
-                                borderType: BorderType.RRect,
-                                color: Colors.black,
-                                strokeWidth: 5,
-                                dashPattern: [20, 10],
-                                radius: Radius.circular(30),
-                                padding: EdgeInsets.all(6),
-                                child: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                        BorderRadius.all(Radius.circular(30)),
-                                        image: DecorationImage(
-                                            image: FileImage(widget.image),
-                                            fit: BoxFit.cover)))))),
-                    Positioned(
-                        top: size.height * .47,
-                        left: size.width * .04,
-                        child: Container(
-                            alignment: Alignment.center,
-                            height: size.height * .1,
-                            width: size.width * (1 / 3),
-                            child: DottedBorder(
-                                borderType: BorderType.RRect,
-                                color: Colors.black,
-                                strokeWidth: 2,
-                                dashPattern: [3, 3],
-                                radius: Radius.circular(10),
-                                padding: EdgeInsets.all(6),
-                                child: Container(
-                                  alignment: Alignment.center,
-                                  height: size.height * .1,
-                                  width: size.width * (1 / 3),
-                                  child: Text(
-                                    '${widget.resultPredict['${_selectedModel} Predicted']}',
-                                    style: TextStyle(
-                                        color: Colors.green,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 13
-                                    ),
-                                    softWrap: true,
-                                  ),
-                                )
-                            )
-                        )
-                    ),
-                    Positioned(
-                        bottom: 0,
-                        child: Container(
-                          alignment: Alignment.bottomCenter,
-                          height: size.width,
+                          height: size.height,
                           width: size.width,
-                          child: ResultsChart(model: _selectedModel, result: widget.resultPredict,),
-                        )),
-                    Positioned(
-                      top: size.height * .05,
-                      right: size.width * .05,
-                      child: Card(
-                        child: Container(
-                          // alignment: Alignment.center,
-                          // width: size.width,
-                            padding: EdgeInsets.symmetric(horizontal: 22, vertical: 2),
-                            decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(.6),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: DropdownButton(
-                              // Not necessary for Option 1
-                              value: _selectedModel,
-                              icon: Icon(Icons.arrow_drop_down_outlined, color: kLightBlackColor, textDirection: TextDirection.ltr,),
-                              iconSize: 30,
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black
-                              ),
-                              underline: SizedBox(),
-                              onChanged: (newValue) {
-                                setState(() {
-                                  _selectedModel = _SelectedModel(newValue);
-                                });
-                              },
-                              items: _Models.map((model) {
-                                return DropdownMenuItem(
-                                  child: new Text(model),
-                                  value: model,
-                                );
-                              }).toList(),
-                            )
+                          decoration: BoxDecoration(
+                              // borderRadius:
+                              // BorderRadius.all(Radius.circular(30)),
+                              image: DecorationImage(
+                                  image: FileImage(widget.image),
+                                  fit: BoxFit.cover)),
                         ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    height: size.height * .7,
+                    width: size.width,
+                    child: ClipPath(
+                      clipper: AppClipper(
+                          cornerSize: 50,
+                          diagonalHeight: 150,
+                          roundedBottom: false),
+                      child: Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.only(top: 100),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          )),
+                Positioned(
+                    bottom: size.height * .43,
+                    right: size.width * .05,
+                    child: Container(
+                        alignment: Alignment.center,
+                        height: size.width * (1.6 / 3),
+                        width: size.width * (1.6 / 3),
+                        child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            color: Colors.black,
+                            strokeWidth: 5,
+                            dashPattern: [20, 10],
+                            radius: Radius.circular(30),
+                            padding: EdgeInsets.all(6),
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(30)),
+                                    image: DecorationImage(
+                                        image: FileImage(widget.image),
+                                        fit: BoxFit.cover)))))),
+                Positioned(
+                    top: size.height * .47,
+                    left: size.width * .04,
+                    child: Container(
+                        alignment: Alignment.center,
+                        height: size.height * .1,
+                        width: size.width * (1 / 3),
+                        child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            color: Colors.black,
+                            strokeWidth: 2,
+                            dashPattern: [3, 3],
+                            radius: Radius.circular(10),
+                            padding: EdgeInsets.all(6),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: size.height * .1,
+                              width: size.width * (1 / 3),
+                              child: Text(
+                                '${widget.resultPredict['${_selectedModelConverted} Predicted']}',
+                                style: TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13),
+                                softWrap: true,
+                              ),
+                            )))),
+                Positioned(
+                    bottom: 0,
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      height: size.width,
+                      width: size.width,
+                      child: ResultsChart(
+                        model: _selectedModelConverted,
+                        result: widget.resultPredict,
+                      ),
+                    )),
+                Positioned(
+                  top: size.height * .05,
+                  right: size.width * .05,
+                  child: Card(
+                    child: Container(
+                        // alignment: Alignment.center,
+                        // width: size.width,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 22, vertical: 2),
+                        decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(.6),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: DropdownButton(
+                          // Not necessary for Option 1
+                          value: _selectedModel,
+                          icon: Icon(
+                            Icons.arrow_drop_down_outlined,
+                            color: kLightBlackColor,
+                            textDirection: TextDirection.ltr,
+                          ),
+                          iconSize: 30,
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          underline: SizedBox(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              _selectedModel = newValue;
+                              _selectedModelConverted =
+                                  _SelectedModel(newValue);
+                            });
+                          },
+                          items: _Models.map((model) {
+                            return DropdownMenuItem(
+                              child: new Text(model),
+                              value: model,
+                            );
+                          }).toList(),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
